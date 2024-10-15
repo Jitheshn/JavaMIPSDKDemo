9fb5f22c9a6a8f42bb084a6bc22882e29a3ca790
Jitheshn <38646937+Jitheshn@users.noreply.github.com>
2024-10-15 10:22:39 +0530

    Add files via upload

---
+++ MIPConsoleApp/src/main/java/com/example/mip/AuthDelegateImpl.java
@@ -0,0 +1,117 @@
+package com.example.mip;
+
+import com.microsoft.aad.msal4j.IAccount;
+import com.microsoft.aad.msal4j.IAuthenticationResult;
+import com.microsoft.aad.msal4j.InteractiveRequestParameters;
+import com.microsoft.aad.msal4j.MsalException;
+import com.microsoft.aad.msal4j.Prompt;
+import com.microsoft.aad.msal4j.PublicClientApplication;
+import com.microsoft.aad.msal4j.SilentParameters;
+import com.microsoft.informationprotection.ApplicationInfo;
+import com.microsoft.informationprotection.IAuthDelegate;
+import com.microsoft.informationprotection.Identity;
+
+
+import java.net.URI;
+import java.util.Collections;
+import java.util.Set;
+
+public class AuthDelegateImpl implements IAuthDelegate {
+
+	private static String TENANT_ID = "<your-tenant-id>";
+    private static String CLIENT_ID = "<your-client-id>";    
+    private static String AUTHORITY = "https://login.microsoftonline.com/" + TENANT_ID;;
+    private static Set<String> SCOPE = Collections.singleton("");
+
+    public AuthDelegateImpl(ApplicationInfo appInfo)
+    {
+        CLIENT_ID = appInfo.getApplicationId();
+    }
+
+    @Override
+    public String acquireToken(Identity userName, String authority, String resource, String claims) {
+        if(resource.endsWith("/")){
+            SCOPE = Collections.singleton(resource + ".default");        
+        }
+        else {
+            SCOPE = Collections.singleton(resource + "/.default");        
+        }
+
+      
+        String token = "";
+        try {
+            token = acquireTokenInteractive().accessToken();
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return token;
+    }
+    
+    private static IAccount getAccountByUsername(Set<IAccount> accounts, String username) {
+        if (accounts.isEmpty()) {
+            System.out.println("==No accounts in cache");
+        } else {
+            System.out.println("==Accounts in cache: " + accounts.size());
+            for (IAccount account : accounts) {
+                if (account.username().equals(username)) {
+                    return account;
+                }
+            }
+        }
+        return null;
+    }
+
+    
+    private static IAuthenticationResult acquireTokenInteractive() throws Exception {
+
+        // Load token cache from file and initialize token cache aspect. The token cache will have
+        // dummy data, so the acquireTokenSilently call will fail.
+        //TokenCacheAspect tokenCacheAspect = new TokenCacheAspect("sample_cache.json");
+
+        PublicClientApplication pca = PublicClientApplication.builder(CLIENT_ID)
+                .authority(AUTHORITY)
+                .build();
+                //.setTokenCacheAccessAspect(tokenCacheAspect)
+                //.build();
+
+        Set<IAccount> accountsInCache = pca.getAccounts().join();
+        // Take first account in the cache. In a production application, you would filter
+        // accountsInCache to get the right account for the user authenticating.
+        IAccount account = null;
+        if(!accountsInCache.isEmpty())
+        {
+        	account = accountsInCache.iterator().next();
+        }
+
+        IAuthenticationResult result;
+        try {
+            SilentParameters silentParameters =
+                    SilentParameters
+                            .builder(SCOPE)
+                            .account(account)
+                            .build();
+
+            // try to acquire token silently. This call will fail since the token cache
+            // does not have any data for the user you are trying to acquire a token for
+            result = pca.acquireTokenSilently(silentParameters).join();
+        } catch (Exception ex) {
+            if (ex.getCause() instanceof MsalException) {
+
+                InteractiveRequestParameters parameters = InteractiveRequestParameters
+                        .builder(new URI("http://localhost"))                        
+                        .prompt(Prompt.SELECT_ACCOUNT) // Change this value to avoid repeated auth prompts. 
+                        .scopes(SCOPE)
+                        .build();
+
+                // Try to acquire a token interactively with system browser. If successful, you should see
+                // the token and account information printed out to console
+                result = pca.acquireToken(parameters).join();
+            } else {
+                // Handle other exceptions accordingly
+                throw ex;
+            }
+        }
+        return result;
+    }
+}
---
+++ MIPConsoleApp/src/main/java/com/example/mip/ConsentDelegate.java
@@ -0,0 +1,13 @@
+package com.example.mip;
+import com.microsoft.informationprotection.Consent;
+import com.microsoft.informationprotection.IConsentDelegate;
+
+
+
+public class ConsentDelegate implements IConsentDelegate {
+
+  @Override
+  public Consent getUserConsent(String url) {
+    return Consent.ACCEPT;
+  }
+}
